714d60190647ac6a7af36f7cc252c626bfc9a8fe
匡俊东 <70204596+233-kuang@users.noreply.github.com>
2021-05-05 23:15:28 +0800

    练习

---
+++ C语音练习01/C语音入门1/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	printf("hello world\n");
+//}
+
+int main()
+{
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(double));
+	printf("%d\n", sizeof(char));
+	return 0;
+
+	/*
+		计算机进制
+		1byte（字节）= 8bite（比特位）
+		二进制转十进制
+		123 = 1*10^2+2*10^1+3*10^0
+	*/
+}
